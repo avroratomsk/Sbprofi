e12b2eeb8c99d29336b93c5438842b0011edbf89
sanchyopas <saniagolovanev@gmail.com>
2025-09-26 17:14:43 +0700

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import * as functions from './modules/baseFunctions.js';
 import './modules/slider.js';
 import './modules/popup.js';
 import './modules/focusInput.js';
+import './modules/stickyMenu.js';
 
 functions.isWebp();
 
